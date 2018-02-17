c97a730a921b173be2c986336e32fa1c912d3aa6
danielmagalhaesit <danielmagalhaesit@gmail.com>
2018-02-17 12:23:08 -0500

    Lab 3 is done

---
+++ Assets/Lab2/ChangeTShirt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTShirt : MonoBehaviour {
+
+    public GameObject AttachedObject;
+    public Sprite Sprite;
+
+    private SpriteRenderer sRend;
+
+    // Use this for initialization
+    void Start()
+    {
+        sRend = AttachedObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        sRend.sprite = Sprite;
+    }
+}
